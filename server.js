819ceed3a16e6d602ccbafbecd54d309714388ad
L.Walker <logantwalker95@gmail.com>
2020-09-23 15:57:10 -0400

    added process.env variable for db connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
-mongoose.connect('mongodb+srv://lwalker37:amv12v@cluster0.p7jgf.mongodb.net/Cluster0?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lwalker37:amv12v@cluster0.p7jgf.mongodb.net/Cluster0?retryWrites=true&w=majority',
 {   useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
